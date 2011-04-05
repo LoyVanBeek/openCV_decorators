da4d6fa1559a1e6f299bbbeee2b35dfebc9a2d15
Loy <loy.vanbeek@gmail.com>
2011-04-05 13:56:32 +0200

    Initial checkin

---
+++ decorators.py
@@ -0,0 +1,113 @@
+"""Library of convienence wrappers for OpenCV-functions and operators."""
+import cv
+
+def make_resources(op):
+    """Preallocate resources image operators need"""
+    def allocator(image, *args, **kwargs):
+        sameSize = True #TODO: make configable
+        depth = image.depth
+        channels = image.channels
+        out = cv.CreateImage(cv.GetSize(image), depth, channels)
+        op(image, out, *args, **kwargs)
+        return out
+    return allocator
+            
+
+#TODO: yielder must be able to handle *arg- and **kwarg-less functions
+def make_filter(op):
+    """"Converts a normal image proccing function into a generator"""
+    def wrapper(*args,**kwargs):
+        while True:
+            image = yield
+            yield op(image, *args, **kwargs)
+
+    return wrapper
+
+def chain(start, *gens):
+    for gen in gens:
+        gen.next()
+    temp = start
+    for gen in gens:
+        temp = gen.send(temp)
+
+    return temp
+
+#does the same as make_filter applied to chain.
+def pipe(*parts):
+    while True:
+        for part in parts:
+            part.next()
+        temp = yield
+        for part in parts:
+            print "Next step: %s"%str(part), temp
+            temp = part.send(temp)
+        yield temp
+
+def split(image):
+    out1 = cv.CreateImage(cv.GetSize(image), cv.IPL_DEPTH_8U, 1)
+    out2 = cv.CreateImage(cv.GetSize(image), cv.IPL_DEPTH_8U, 1)
+    out3 = cv.CreateImage(cv.GetSize(image), cv.IPL_DEPTH_8U, 1)
+    if image.nChannels ==4:
+        out4 = cv.CreateImage(cv.GetSize(image), cv.IPL_DEPTH_8U, 1)
+    else:
+        out4 = None
+
+    cv.Split(image, out1, out2, out3, out4)
+
+    return out1, out2, out3, out4
+
+def merge(*channels):
+    debug=False
+    assert len(channels) > 0
+
+    firstDepth = channels[0].depth
+    assert all([channel.depth == firstDepth for channel in channels])
+
+    firstSize = cv.GetSize(channels[0])
+    assert all([cv.GetSize(channel) == firstSize for channel in channels])
+
+    chans = 3 if len(channels) == 2 else len(channels) #For filling up the 3rd chan when there are 2 imgs, add room for this 3rd channel
+    if debug: print "merging %i channels"%chans
+    out = cv.CreateImage(cv.GetSize(channels[0]), channels[0].depth, chans)
+    #print out
+    
+    if len(channels) == 4:
+        cv.Merge(channels[0],channels[1],channels[2],channels[3],out)
+    elif len(channels) == 3:
+        cv.Merge(channels[0],channels[1],channels[2],None,out)
+    elif len(channels) == 2:
+        filler = cv.CreateImage(cv.GetSize(channels[0]), channels[0].depth, 1)
+        cv.Merge(channels[0],channels[1],filler,None,out) #TODO: make pos of filler configable
+    elif len(channels) == 1:
+        cv.Merge(channels[0],None,None,None,out)
+    return out
+
+def apply_to_channels(image, ops, merge_result=False, out=None, debug=False):
+    if debug: print "Splitting image.nChannels=%i"%image.nChannels
+    channels = split(image)
+    
+    if debug: print "Applying operation to %s channels"%str(channels)
+    parts = [chain(channel, *ops) for channel in channels if channel]
+
+    if debug: print "Merging operated channels"
+    result = merge(*parts)
+    if debug: print "Done merging"
+##    if not out:
+##        return result
+##    else:
+##        return out
+    return result
+
+def test1(image):
+    agauss = make_resources(cv.Smooth)
+    return agauss(image, cv.CV_GAUSSIAN, 11,11)
+    
+
+if __name__ == "__main__":
+    image = cv.LoadImage("""images\geilo_25.png""")
+
+    out = test1(image)
+    cv.NamedWindow("Show")
+    cv.ShowImage("Show", out)
+    cv.WaitKey()
+    cv.DestroyAllWindows()
---
+++ opencv_wrappers.py
@@ -0,0 +1,91 @@
+"""decorators around OpenCV functions to make them more pythonic and
+automatically allocate resources functions need"""
+
+import cv
+import decorators
+
+def show(**images):
+    for name, im in images.iteritems():
+        cv.NamedWindow(name)
+        cv.ShowImage(name, im)
+
+    cv.WaitKey(0)
+    for name in images.keys():
+        cv.DestroyWindow(name)
+
+@decorators.make_filter
+def gauss(im, size=(11,11), method=cv.CV_GAUSSIAN, out=None):
+    assert (size[0] % 2 == 1) and (size[1] % 2 == 1)
+
+    if not out: out = cv.CreateImage(cv.GetSize(im), cv.IPL_DEPTH_8U, im.channels)
+
+    assert cv.GetSize(im) == cv.GetSize(out)
+    assert im.depth == out.depth
+    assert im.channels == out.channels
+    cv.Smooth(im, out, method, size[0],size[1])
+
+    return out
+
+@decorators.make_filter
+def scale(im, scale=2, imfilter=cv.CV_GAUSSIAN_5x5, out=None):
+    assert im.width%scale == 0 and im.height%scale == 0
+    
+    if not out: out = cv.CreateImage((im.width/scale, im.height/scale), im.depth, im.channels)
+
+    cv.PyrDown(im, out)
+
+    return out
+
+@decorators.make_filter
+def doCanny(im, low, high, aperture, out=None):
+    in1 = cv.CreateImage(cv.GetSize(im), cv.IPL_DEPTH_8U, 1)
+    in2 = cv.CreateImage(cv.GetSize(im), cv.IPL_DEPTH_8U, 1)
+    in3 = cv.CreateImage(cv.GetSize(im), cv.IPL_DEPTH_8U, 1)
+
+    cv.Split(im, in1, in2, in3, None)
+
+    out1 = doCanny_single(in1, low, high, aperture)
+    out2 = doCanny_single(in2, low, high, aperture)
+    out3 = doCanny_single(in3, low, high, aperture)
+
+    if not out: out = cv.CreateImage(cv.GetSize(im), cv.IPL_DEPTH_8U, 3)
+    cv.Merge(out1, out2, out3, None, out)
+
+    return out
+
+@decorators.make_filter
+def doCanny_single(im, low, high, aperture):
+    assert im.channels == 1
+
+    out = cv.CreateImage(cv.GetSize(im), cv.IPL_DEPTH_8U, 1)
+
+    cv.Canny(im, out, low, high, aperture)
+    return out
+
+############# TESTS #############
+def test1(image):
+    pipe = decorators.chain(gauss(), scale())
+    pipe.next()
+    out = pipe.send(image)
+    #gaussed = decorators.chain(image, gauss())
+    show(gaussed=out, orig=image)
+
+def test2(image):
+    pipe2 = decorators.pipe(gauss(), scale())
+    print "pipe2 created"
+    pipe2.next()
+    print "Initialized"
+    out2 = pipe2.send(image)
+    print "Called, showing output:"
+    show(out2=out2)
+
+def test3(image):
+    canny = decorators.apply_to_channels(image, (gauss(), doCanny_single(0,255,3)))
+    show(cannied=canny)
+
+if __name__ == "__main__":
+    image = cv.LoadImage("""images\geilo_25.png""")
+
+##    test1(image)
+##    test2(image)
+    test3(image)
